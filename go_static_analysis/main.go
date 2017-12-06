9ae0387098d1a1cc902b1754eb5205a947624418
Tan Nguyen <tan.nqd@gmail.com>
2017-12-06 14:13:03 -0800

    remove misleading comments

--- go_static_analysis/main.go
+++ go_static_analysis/main.go
@@ -1,30 +1,3 @@
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// godoc: Go Documentation Server
-
-// Web server tree:
-//
-//	http://godoc/		main landing page
-//	http://godoc/doc/	serve from $GOROOT/doc - spec, mem, etc.
-//	http://godoc/src/	serve files from $GOROOT/src; .go gets pretty-printed
-//	http://godoc/cmd/	serve documentation about commands
-//	http://godoc/pkg/	serve documentation about packages
-//				(idea is if you say import "compress/zlib", you go to
-//				http://godoc/pkg/compress/zlib)
-//
-// Command-line interface:
-//
-//	godoc packagepath [name ...]
-//
-//	godoc compress/zlib
-//		- prints doc for package compress/zlib
-//	godoc crypto/block Cipher NewCMAC
-//		- prints doc for Cipher and NewCMAC in package crypto/block
-
-// +build !appengine
-
 package main
 
 import (
@@ -45,5 +18,3 @@ func main() {
   analysis.Run(pointerAnalysis, file)
   fmt.Println("Done")
 }
-
-
